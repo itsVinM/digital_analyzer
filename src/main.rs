8147b052083f800051f0682b3d1686f57b0a6e0c
itsVinM <vincentiu.mocanu00@gmail.com>
2025-11-29 17:29:47 +0100

    Initial commit: Digital Twin Rust/C

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use panic_halt as _;
+use smt32f4xx_hal::{
+    i2c::Mode as I2cMode,
+    can::Mode as CanMode,
+    spi::Mode as SpiMode,
+    usb::Mode as UsbMode,
+    gpio::*,
+    pac::{self},
+    prelude::*,
+    serial::{config::Config, Mode as SerialMode}
+};
+
+#[entry]
+fn main() -> !{
+
+}
